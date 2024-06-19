1ae993f488c1c51311337178e5440c6f1ded148b
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-06-19 19:17:51 +0530

    completed building Meeting Room

---
+++ components/EndCallButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+
+const EndCallButton = () => {
+  const call = useCall();
+  const router = useRouter();
+
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticipant &&
+    call?.state.createdBy &&
+    localParticipant.userId === call.state.createdBy.id;
+
+  if (!isMeetingOwner) return null;
+
+  return (
+    <Button
+      onClick={async () => {
+        await call.endCall();
+        router.push(`/`);
+      }}
+      className="bg-red-500"
+    >
+      End Call for Everyone
+    </Button>
+  );
+};
+
+export default EndCallButton;
